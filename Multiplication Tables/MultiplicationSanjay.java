ff9254ad3941948288f36d7faf1bae77a57a0aa1
Sanjay <72803056+saisanjaypotluri@users.noreply.github.com>
2023-12-23 19:31:25 -0500

    Add files via upload

---
+++ Multiplication Tables/MultiplicationSanjay.java
@@ -0,0 +1,27 @@
+package Class;
+
+import java.util.Scanner;
+
+public class MultiplicationSanjay
+{
+	public static void main (String[] args)
+	{
+		char retry = 'Y';
+		while (retry == 'Y')
+		{
+			int i = 1;
+			Scanner input = new Scanner(System.in);
+			System.out.printf("Enter number for its Multiplication Table: ");
+			int number = input.nextInt(); 
+			System.out.print("Enter the length of the table; ");
+			int length = input.nextInt();
+			for (i = 1; i <= length; i++)
+			{
+				System.out.printf("%d x %d = %d\n", number, i, number*i);
+			}
+			System.out.print("Do you wish to check another table?[Y/N]: ");
+			retry = input.next().charAt(0);
+		}
+		System.out.println("Program exit!");
+	}
+}
